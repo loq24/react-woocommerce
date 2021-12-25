da207bb19d97b44a0d564cb1e0539a6f1512a4ea
Lougie Quisel <lougie.quisel@gmail.com>
2021-12-25 10:14:08 +0800

    Add empty cart text (#3)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["react-ecommerce.local"]
+    domains: ["react-ecommerce.local", "react-ecommerce-v2.lougiequisel.com"]
   }
 };
--- src/components/PageLayout/Cart/Cart.tsx
+++ src/components/PageLayout/Cart/Cart.tsx
@@ -39,39 +39,48 @@ const Cart = () => {
       <div
         className={classNames(
           "fixed sm:w-96 w-full h-screen right-0 z-30 bg-black top-14 overflow-hidden",
-          { hidden: !cartVisibility }
+          { hidden: !cartVisibility },
+          { "flex flex-col items-center justify-center": cart.length === 0 }
         )}
       >
-        <div className="relative h-full">
-          <div className="relative w-full h-2/3 p-5 overflow-y-auto top-0">
-            <h4 className="text-3xl text-white font-medium mb-8">My Cart</h4>
-            {cart && <ItemList products={cart} />}
+        {cart.length > 0 ? (
+          <div className="relative h-full">
+            <div className="relative w-full h-2/3 p-5 overflow-y-auto top-0">
+              <h4 className="text-3xl text-white font-medium mb-8">My Cart</h4>
+              {cart && <ItemList products={cart} />}
+            </div>
+            <div className="w-full sticky h-80 bg-black -ml-2.5 border-t border-white p-6 pl-8 bottom-0">
+              <div className="flex flex-wrap flex-row justify-between mb-4">
+                <span className="text-white text-sm">Subtotal</span>
+                <span className="text-white text-sm">${subTotal}</span>
+              </div>
+              <div className="flex flex-wrap flex-row justify-between mb-4">
+                <span className="text-white text-sm">Taxes</span>
+                <span className="text-white text-sm">
+                  Calculated at checkout
+                </span>
+              </div>
+              <div className="flex flex-wrap flex-row justify-between mb-4">
+                <span className="text-white text-sm">Shipping</span>
+                <span className="text-white text-sm">FREE</span>
+              </div>
+              <div className="w-full h-px bg-gray-500 mb-4"></div>
+              <div className="flex flex-wrap flex-row justify-between mb-4">
+                <span className="text-white text-sm font-semibold">Total</span>
+                <span className="text-white text-sm font-semibold">
+                  ${subTotal}
+                </span>
+              </div>
+              <button className="bg-white border-0 py-4 w-full text-sm uppercase hover:bg-gray-300 transition duration-500 ease-in-out">
+                Proceed To Checkout
+              </button>
+            </div>
           </div>
-          <div className="w-full sticky h-80 bg-black -ml-2.5 border-t border-white p-6 pl-8 bottom-0">
-            <div className="flex flex-wrap flex-row justify-between mb-4">
-              <span className="text-white text-sm">Subtotal</span>
-              <span className="text-white text-sm">${subTotal}</span>
-            </div>
-            <div className="flex flex-wrap flex-row justify-between mb-4">
-              <span className="text-white text-sm">Taxes</span>
-              <span className="text-white text-sm">Calculated at checkout</span>
-            </div>
-            <div className="flex flex-wrap flex-row justify-between mb-4">
-              <span className="text-white text-sm">Shipping</span>
-              <span className="text-white text-sm">FREE</span>
-            </div>
-            <div className="w-full h-px bg-gray-500 mb-4"></div>
-            <div className="flex flex-wrap flex-row justify-between mb-4">
-              <span className="text-white text-sm font-semibold">Total</span>
-              <span className="text-white text-sm font-semibold">
-                ${subTotal}
-              </span>
-            </div>
-            <button className="bg-white border-0 py-4 w-full text-sm uppercase hover:bg-gray-300 transition duration-500 ease-in-out">
-              Proceed To Checkout
-            </button>
-          </div>
-        </div>
+        ) : (
+          <h4 className=" text-white text-center font-medium mb-8 text-base">
+            Your cart is empty.
+          </h4>
+        )}
       </div>
     </>
   );
