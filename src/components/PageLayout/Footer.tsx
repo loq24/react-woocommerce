e2dc513fc62ebec15ec73d5eddba56f9865f1394
Lougie <lougie.quisel@gmail.com>
2021-12-28 12:28:25 +0800

    Update footer text

--- src/components/PageLayout/Footer.tsx
+++ src/components/PageLayout/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="w-full py-3 mt-20 mb-10 ">
       <p className="text-sm text-gray-400 text-center">
-        &copy; 2021 NextJS eCommerce. All rights reserved.
+        &copy; 2021 React WooCommerce. All rights reserved.
       </p>
     </div>
   );
